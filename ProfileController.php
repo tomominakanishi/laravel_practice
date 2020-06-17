fe57cea4c67f9ff2a840eec3894e38152dc2d58b
tomominakanishi <tomomin1030@gmail.com>
2020-06-17 10:25:53 +0900

    fixed1

--- ProfileController.php
+++ ProfileController.php
@@ -8,28 +8,23 @@ use App\Http\Controllers\Controller;
 class ProfileController extends Controller
 {
   public function add()
-  {
+{
     return view('admin.profile.create');
-  }
+}
 
   public function create()
-  {
+{
     return redirect('admin/profile/create');
-  }
+}
 
   public function edit()
-  {
-    return view('admin.profile.adit');
-  }
+{
+    return view('admin.profile.edit');
+}
 
   public function update()
-  {
+{
     return redirect('admin/profile/edit');
-  }
+}
 
-    Route::group(['prefix' => 'admin'], function() {
-    Route::get('profile/create', 'Admin\ProfileController@add');
-    Route::get('profile/edit','Admin\ProfileController'@edit);
-    
-});
 }
